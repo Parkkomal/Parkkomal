6911acf3ab7db0f46cd76dc6e46e9fb2d8e6d126
Parkkomal <78652251+Parkkomal@users.noreply.github.com>
2021-02-06 20:41:34 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"601ead8b179f5c00220691a8",
+				"601ead8b179f5c00220691aa"
+			]
+		},
+		"601ead8b179f5c00220691a8": {
+			"id": "601ead8b179f5c00220691a8",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"601ead8b179f5c00220691aa": {
+			"id": "601ead8b179f5c00220691aa",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
